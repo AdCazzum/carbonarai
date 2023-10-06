53345071a7015b3acbd9de8e20b98b6b8c9ee0d9
Ivan Sala <slavni96@gmail.com>
2023-10-06 21:43:10 +0200

    Added Navbar

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { IconExternalLink } from "@tabler/icons-react";
+import { FC } from "react";
+
+export const Navbar: FC = () => {
+  return (
+    <div className="flex h-[60px] border-b border-gray-300 py-2 px-8 items-center justify-between">
+      <div className="font-bold text-2xl flex items-center">
+        <a
+          className="hover:opacity-50"
+          href="/"
+        >
+          <div className="inline-flex items-center">
+            CarbonarAI 🍝
+          </div>
+        </a>
+      </div>
+      <div>
+        <a
+          className="flex items-center hover:opacity-50"
+          href="https://github.com/slavni96/carbonarai"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {/* <div className="hidden sm:flex">beatdata.it</div> */}
+          <img src={`https://ethrome.org/ethrome_logo.png`} style={{ height: "3rem" }} />
+          {/* 
+          <IconExternalLink
+            className="ml-1"
+            size={20}
+          /> */}
+        </a>
+      </div>
+    </div>
+  );
+};
